447419d67a626932e09ec4bffe249a79081acfa9
mahyarjbr <mahyarjbr1374@gmail.com>
2022-08-15 16:30:38 +0430

    adding all

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const authRoutes = require("./routes/authRoutes");
+const mongoose=require("mongoose")
+require("./services/passport");
+
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+
+
+mongoose.connect("mongodb://localhost:27017").then(() =>{
+    console.log("db connected")
+}).catch((err) =>{
+    console.log(err)
+
+})
+
+
+app.get("/", (req, res) => {
+    res.send("home page")
+})
+
+authRoutes(app);
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        console.log(`server is runnig on PORT ${PORT}`)
+    }
+})
