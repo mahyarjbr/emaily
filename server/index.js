7c0afe70253efac3c46a0432146444b3204c799d
mahyarjbr <mahyarjbr1374@gmail.com>
2022-08-15 16:49:11 +0430

    شششش

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ mongoose.connect("mongodb://localhost:27017").then(() =>{
     console.log("db connected")
 }).catch((err) =>{
     console.log(err)
+    console.log(err)
+    console.log(err)
+    console.log(err)
 
 })
 
